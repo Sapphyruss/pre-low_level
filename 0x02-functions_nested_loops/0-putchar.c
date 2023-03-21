1f5dcd6a2405f0b6af67b2863d98a2014f418364
Sapphyruss <lugi.franklin@outlook.fr>
2023-03-21 09:31:30 -0700

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include "stdio.h"
+#include "main.h"
+
 /**
- * main -Entry point
+ * main - program that print _putchar
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 
 int main(void)
 {
+	char str[8] = "_putchar";
+	int i = 0;
 
-	char str[] = "_putchar\n";
-	int i;
-
-	for (i = 0; i <= 8; i++)
+	while (str[i])
 	{
 		_putchar(str[i]);
+		i++;
 	}
-
+	_putchar('\n');
 	return (0);
-
 }
