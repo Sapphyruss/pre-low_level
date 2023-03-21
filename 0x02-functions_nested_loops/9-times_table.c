f37073a1aecf2fb9a44b824c991f1baa1a45c4e6
Sapphyruss <lugi.franklin@outlook.fr>
2023-03-21 12:29:44 -0700

    .

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,35 @@
-#include "main.h"
+#include "coding.h"
 
 /**
- * add - is a function that adds two ints and return its result
- * Return: the added value of two ints
- * @first: first int
- * @second: second int
+ * times_table - function without parameter
+ *
+ * Description: using for loops to create table and multiples
+ * Return: result
  */
-
-int add(int first, int second)
+void times_table(void)
 {
-	return (first + second);
+	int i, j, result;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
+		{
+			result = i * j;
+			if (result < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(result + '0');
+			} else if (result >= 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(result / 10 + '0');
+				_putchar(result % 10 + '0');
+			}
+		}
+		_putchar('\n');
+	}
 }
