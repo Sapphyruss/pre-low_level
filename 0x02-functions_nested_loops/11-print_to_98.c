32360ea9caeee10e7feda043c5bc98554408b025
Sapphyruss <lugi.franklin@outlook.fr>
2023-03-21 12:36:00 -0700

    .

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * print_to_98 - function with one argument
+ * @n: first operand
+ *
+ * Description: Print all natural numbers from n to 98
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (; n >= 98; n--)
+		{
+			printf("%i", n);
+			if (n != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	} else if (n < 98)
+	{
+		for (; n <= 98; n++)
+		{
+			printf("%i", n);
+			if (n != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	} else
+	{
+		printf("%i", n);
+	}
+	putchar('\n');
+}
