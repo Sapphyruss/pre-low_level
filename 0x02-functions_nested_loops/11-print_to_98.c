76e3319574be661c0ea97cf7dd061590bfcfb26e
Sapphyruss <lugi.franklin@outlook.fr>
2023-03-21 12:39:19 -0700

    .

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,31 +8,17 @@
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	if (n >= 98)
 	{
-		for (; n >= 98; n--)
-		{
-			printf("%i", n);
-			if (n != 98)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	} else if (n < 98)
-	{
-		for (; n <= 98; n++)
-		{
-			printf("%i", n);
-			if (n != 98)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	} else
-	{
-		printf("%i", n);
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
 	}
-	putchar('\n');
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
 }
