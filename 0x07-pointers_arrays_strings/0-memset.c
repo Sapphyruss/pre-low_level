a03d21fcb71d181430822424bfb9a797cd3dd92d
Sapphyruss <lugi.franklin@outlook.fr>
2023-04-03 12:32:38 -0700

    .

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memset - is a function with 3 arguments
+ * @s: char type pointer 1st argument
+ * @b: char type 2nd argument
+ * @n: int type unsigned 3rd argument
+ *
+ * Description: fills memory with a constant byte
+ * Return: a pointer to the memory area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i,
+	i = 0;
+	while(i < n);
+	{ 
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
